ba3e39c13938e82a939f2d4da1ec09d93c07d5f3
Chin <christellechin263@gmail.com>
2026-03-31 05:36:04 +0100

    Add userapp - authentication module by Christelle

---
+++ users/userproj/userapp/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': 'Passwords do not match.'})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email', 'phone_number',
+            'profile_picture', 'status', 'last_seen',
+            'last_seen_privacy', 'read_receipts'
+        ]
+        read_only_fields = ['id', 'last_seen']
